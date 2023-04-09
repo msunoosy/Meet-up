e772dcd0947cf1c351acbb66bca40107a3a93f68
msunoosy <m.sunoos@gmail.com>
2023-04-09 21:48:14 +0530

    Favorite and remove favorite function added

--- src/Pages/FavouriteMeetups.js
+++ src/Pages/FavouriteMeetups.js
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import FavoritesContext from '../store/favorites-context'
+import Meetuplist from '../Components/meetups/Meetuplist';
 export default function FavouriteMeetups() {
+
+  const favoritesCtx=useContext(FavoritesContext)
+  
+  let content; 
+  if (favoritesCtx.totalFavorites===0){
+    content=<p>No Favorites Available</p>
+  }else{
+    content=<Meetuplist meetups={favoritesCtx.favorites}></Meetuplist>
+  }
+
+    
   return (
-    <div>Favourite Meetups</div>
+    <section>
+    <h1>Favourite Meetups</h1> 
+    {content}
+   
+    </section>
   )
 }
